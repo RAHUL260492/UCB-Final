c6b31293f8eca5a008428f7bdf4f52cf53adf391
Rahul Gupta <rahulgupta@192.168.1.50>
2026-02-15 20:16:49 +0530

    chore: setup github pages workflow

--- vite.config.ts
+++ vite.config.ts
@@ -18,6 +18,7 @@ export default defineConfig(({ mode }) => {
       'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
       'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
     },
+    base: '/UCB-Final/',
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
