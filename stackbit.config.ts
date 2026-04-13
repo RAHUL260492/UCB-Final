e28c7ee409ab93a31528af75879054cf593d4f1a
Rahul Gupta <rahulgupta@192.168.1.25>
2026-04-13 23:46:19 +0530

    Add basic Stackbit configuration

---
+++ stackbit.config.ts
@@ -0,0 +1,8 @@
+import { defineStackbitConfig } from '@stackbit/types';
+
+export default defineStackbitConfig({
+    stackbitVersion: '~0.6.0',
+    ssgName: 'custom', 
+    contentSources: [],
+    modelExtensions: []
+});
